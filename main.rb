a652859ce323fe2c101522d8d69f81cf6176f73b
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-10-22 21:06:59 +0100

    updating

--- main.rb
+++ main.rb
@@ -3,10 +3,20 @@ require_relative 'die.rb'
 require_relative 'kangaroo.rb'
 require_relative 'grid.rb'
 class Main
+ grid=Grid.new
 
-  grid= Grid.new
   puts 'Enter dimension of the Grid for Part I(>=1):'
   grid.dimension = gets.to_i
+
+  dice=Die.new
+
+
+
+  45.times do  dice.throw
+
+  end
+  dice.to_s
+ #   print 'Enter the dimension for grid for PartII(>=1):'
 
 
 end
