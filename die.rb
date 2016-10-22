643fbc32b12cf8cc8331acb6ef3b7534238a30d4
OrlaC15 <orla.cullen@ucdconnect.ie>
2016-10-22 21:01:02 +0100

    updating

--- die.rb
+++ die.rb
@@ -1,19 +1,49 @@
 class Die
- def initialize
-   
- end
+  attr_accessor :side
 
-  def throw!
-    number = Random.new.rand(1..4)
-    case
-      when 1 then North
-      when 2 then South
-      when 3 then East
-      when 4  then West
+  def initialize
+    @side
+    @total= 0
+    @table= {:north => 0, :east => 0, :south => 0, :west => 0}
+  end
+
+  def roll
+    @total= @total+1
+    number = Random.rand(4)
+
+  end
+
+  def throw
+
+    number = roll
+    case number
+      when 1
+        @side = :north
+        @table[:north]+=1
+      when 2
+        @side = :east
+        @table[:east]+=1
+      when 3
+        @side = :south
+        @table[:south]+=1
+      else
+        @side = :west
+        @table[:west]+=1
 
     end
   end
 
+  def print_table
 
+    @table.each { |key, value| puts "#{key} is #{value}" }
+  end
+
+  def total_throws
+    @total
+  end
+
+  def average
+    total_throws*100
+  end
 
 end
